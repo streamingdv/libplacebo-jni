e901f7cc7ae730e7978b6e7bff29a51cc731e696
grill2010 <f.grill@live.at>
2024-01-06 12:50:45 +0100

    +) Update placebo_jni.cpp

--- libplacebo-jni-native/src/placebo_jni.cpp
+++ libplacebo-jni-native/src/placebo_jni.cpp
@@ -788,7 +788,7 @@ void render_ui(struct ui *ui) {
          auto it = imageViewMap.find(buttonType);
          if (it != imageViewMap.end()) {
              VkImageView imageView = it->second;
-             struct nk_image btnImage = nk_image_ptr(reinterpret_cast<void*>(static_cast<uint64_t>(&imageView)));
+             struct nk_image btnImage = nk_image_ptr(reinterpret_cast<void*>(imageView));
 
              nk_layout_space_push(ctx, nk_rect(100, 600, 64, 64));
              nk_button_image(ctx, btnImage);
