10181b1568a06b5b502e6e43b53a05c062df863a
grill2010 <f.grill@live.at>
2024-01-06 01:22:11 +0100

    +) Update placebo_jni.cpp

--- libplacebo-jni-native/src/placebo_jni.cpp
+++ libplacebo-jni-native/src/placebo_jni.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <array>
 #include <jni.h>
 
 #ifdef _WIN32
@@ -72,6 +73,15 @@ enum class ButtonType {
   SHARE,
   FULLSCREEN,
   CLOSE
+};
+
+const std::array<ButtonType, 3> buttonTypes = {
+    ButtonType::MIC,
+    ButtonType::OPTIONS,
+    ButtonType::PS,
+    ButtonType::SHARE,
+    ButtonType::FULLSCREEN,
+    ButtonType::CLOSE
 };
 
 std::map<ButtonType, VkImageView> imageViewMap;
@@ -553,237 +563,258 @@ cleanup:
 extern "C"
 JNIEXPORT void JNICALL Java_com_grill_placebo_PlaceboManager_plTextDestroy
   (JNIEnv *env, jobject obj, jlong placebo_vulkan) {
-    pl_vulkan vulkan = reinterpret_cast<pl_vulkan>(placebo_vulkan);
-    for (int i = 0; i < 4; i++) {
-        if (placebo_tex_global[i])
-            pl_tex_destroy(vulkan->gpu, &placebo_tex_global[i]);
-    }
+  pl_vulkan vulkan = reinterpret_cast<pl_vulkan>(placebo_vulkan);
+  for (int i = 0; i < 4; i++) {
+      if (placebo_tex_global[i])
+          pl_tex_destroy(vulkan->gpu, &placebo_tex_global[i]);
+  }
 }
 
 /**** create UI methods ****/
 
 struct ui_vertex {
-    float pos[2];
-    float coord[2];
-    uint8_t color[4];
+  float pos[2];
+  float coord[2];
+  uint8_t color[4];
 };
 
 #define NUM_VERTEX_ATTRIBS 3
 
 struct ui {
-    pl_gpu gpu;
-    pl_dispatch dp;
-    struct nk_context nk;
-    struct nk_font_atlas atlas;
-    struct nk_buffer cmds, verts, idx;
-    pl_tex font_tex;
-    struct pl_vertex_attrib attribs_pl[NUM_VERTEX_ATTRIBS];
-    struct nk_draw_vertex_layout_element attribs_nk[NUM_VERTEX_ATTRIBS+1];
-    struct nk_convert_config convert_cfg;
+  pl_gpu gpu;
+  pl_dispatch dp;
+  struct nk_context nk;
+  struct nk_font_atlas atlas;
+  struct nk_buffer cmds, verts, idx;
+  pl_tex font_tex;
+  struct pl_vertex_attrib attribs_pl[NUM_VERTEX_ATTRIBS];
+  struct nk_draw_vertex_layout_element attribs_nk[NUM_VERTEX_ATTRIBS+1];
+  struct nk_convert_config convert_cfg;
 };
 
 void ui_destroy(struct ui *ui)
 {
-    if (!ui)
-        return;
+  if (!ui)
+      return;
 
-    nk_buffer_free(&ui->cmds);
-    nk_buffer_free(&ui->verts);
-    nk_buffer_free(&ui->idx);
-    nk_free(&ui->nk);
-    nk_font_atlas_clear(&ui->atlas);
-    pl_tex_destroy(ui->gpu, &ui->font_tex);
-    pl_dispatch_destroy(&ui->dp);
+  nk_buffer_free(&ui->cmds);
+  nk_buffer_free(&ui->verts);
+  nk_buffer_free(&ui->idx);
+  nk_free(&ui->nk);
+  nk_font_atlas_clear(&ui->atlas);
+  pl_tex_destroy(ui->gpu, &ui->font_tex);
+  pl_dispatch_destroy(&ui->dp);
 
-    delete ui;
+  delete ui;
 }
 
 struct ui *ui_create(pl_gpu gpu)
 {
-    struct ui *ui = new struct ui;
-    if (!ui)
-        return NULL;
+  struct ui *ui = new struct ui;
+  if (!ui)
+      return NULL;
 
-    *ui = (struct ui) {
-        .gpu = gpu,
-        .dp = pl_dispatch_create(gpu->log, gpu),
-        .attribs_pl = {
-            {
-                .name = "pos",
-                .fmt = pl_find_vertex_fmt(gpu, PL_FMT_FLOAT, 2),
-                .offset = offsetof(struct ui_vertex, pos),
-            }, {
-                .name = "coord",
-                .fmt = pl_find_vertex_fmt(gpu, PL_FMT_FLOAT, 2),
-                .offset = offsetof(struct ui_vertex, coord),
-            }, {
-                .name = "vcolor",
-                .fmt = pl_find_named_fmt(gpu, "rgba8"),
-                .offset = offsetof(struct ui_vertex, color),
-            }
-        },
-        .attribs_nk = {
-            {NK_VERTEX_POSITION, NK_FORMAT_FLOAT, offsetof(struct ui_vertex, pos)},
-            {NK_VERTEX_TEXCOORD, NK_FORMAT_FLOAT, offsetof(struct ui_vertex, coord)},
-            {NK_VERTEX_COLOR, NK_FORMAT_R8G8B8A8, offsetof(struct ui_vertex, color)},
-            {NK_VERTEX_LAYOUT_END}
-        },
-        .convert_cfg = {
-            .global_alpha = 1.0f,
-            .line_AA = NK_ANTI_ALIASING_ON,
-            .shape_AA = NK_ANTI_ALIASING_ON,
-            .circle_segment_count = 22,
-            .arc_segment_count = 22,
-            .curve_segment_count = 22,
-            .vertex_layout = ui->attribs_nk,
-            .vertex_size = sizeof(struct ui_vertex),
-            .vertex_alignment = NK_ALIGNOF(struct ui_vertex),
-        }
-,
-    };
+  *ui = (struct ui) {
+      .gpu = gpu,
+      .dp = pl_dispatch_create(gpu->log, gpu),
+      .attribs_pl = {
+          {
+              .name = "pos",
+              .fmt = pl_find_vertex_fmt(gpu, PL_FMT_FLOAT, 2),
+              .offset = offsetof(struct ui_vertex, pos),
+          }, {
+              .name = "coord",
+              .fmt = pl_find_vertex_fmt(gpu, PL_FMT_FLOAT, 2),
+              .offset = offsetof(struct ui_vertex, coord),
+          }, {
+              .name = "vcolor",
+              .fmt = pl_find_named_fmt(gpu, "rgba8"),
+              .offset = offsetof(struct ui_vertex, color),
+          }
+      },
+      .attribs_nk = {
+          {NK_VERTEX_POSITION, NK_FORMAT_FLOAT, offsetof(struct ui_vertex, pos)},
+          {NK_VERTEX_TEXCOORD, NK_FORMAT_FLOAT, offsetof(struct ui_vertex, coord)},
+          {NK_VERTEX_COLOR, NK_FORMAT_R8G8B8A8, offsetof(struct ui_vertex, color)},
+          {NK_VERTEX_LAYOUT_END}
+      },
+      .convert_cfg = {
+          .global_alpha = 1.0f,
+          .line_AA = NK_ANTI_ALIASING_ON,
+          .shape_AA = NK_ANTI_ALIASING_ON,
+          .circle_segment_count = 22,
+          .arc_segment_count = 22,
+          .curve_segment_count = 22,
+          .vertex_layout = ui->attribs_nk,
+          .vertex_size = sizeof(struct ui_vertex),
+          .vertex_alignment = NK_ALIGNOF(struct ui_vertex),
+      }
 
-    // Initialize font atlas using built-in font
-    nk_font_atlas_init_default(&ui->atlas);
-    nk_font_atlas_begin(&ui->atlas);
-    struct nk_font *font = nk_font_atlas_add_default(&ui->atlas, 20, NULL);
-    struct pl_tex_params tparams = {
-        .format = pl_find_named_fmt(gpu, "r8"),
-        .sampleable = true,
-        .initial_data = nk_font_atlas_bake(&ui->atlas, &tparams.w, &tparams.h,
-                                           NK_FONT_ATLAS_ALPHA8),
-    };
-    ui->font_tex = pl_tex_create(gpu, &tparams);
-    nk_font_atlas_end(&ui->atlas, nk_handle_ptr((void *) ui->font_tex),
-                      &ui->convert_cfg.tex_null);
-    nk_font_atlas_cleanup(&ui->atlas);
+  };
 
-    if (!ui->font_tex)
-        goto error;
+  // Initialize font atlas using built-in font
+  nk_font_atlas_init_default(&ui->atlas);
+  nk_font_atlas_begin(&ui->atlas);
+  struct nk_font *font = nk_font_atlas_add_default(&ui->atlas, 20, NULL);
+  struct pl_tex_params tparams = {
+      .format = pl_find_named_fmt(gpu, "r8"),
+      .sampleable = true,
+      .initial_data = nk_font_atlas_bake(&ui->atlas, &tparams.w, &tparams.h,
+                                         NK_FONT_ATLAS_ALPHA8),
+  };
+  ui->font_tex = pl_tex_create(gpu, &tparams);
+  nk_font_atlas_end(&ui->atlas, nk_handle_ptr((void *) ui->font_tex),
+                    &ui->convert_cfg.tex_null);
+  nk_font_atlas_cleanup(&ui->atlas);
 
-    // Initialize nuklear state
-    if (!nk_init_default(&ui->nk, &font->handle)) {
-        LogCallbackFunction(nullptr, PL_LOG_ERR, "NK: failed initializing UI!");
-        goto error;
-    }
+  if (!ui->font_tex)
+      goto error;
 
-    nk_buffer_init_default(&ui->cmds);
-    nk_buffer_init_default(&ui->verts);
-    nk_buffer_init_default(&ui->idx);
+  // Initialize nuklear state
+  if (!nk_init_default(&ui->nk, &font->handle)) {
+      LogCallbackFunction(nullptr, PL_LOG_ERR, "NK: failed initializing UI!");
+      goto error;
+  }
 
-    return ui;
+  nk_buffer_init_default(&ui->cmds);
+  nk_buffer_init_default(&ui->verts);
+  nk_buffer_init_default(&ui->idx);
+
+  return ui;
 
 error:
-    ui_destroy(ui);
-    return NULL;
+  ui_destroy(ui);
+  return NULL;
 }
 
 bool ui_draw(struct ui *ui, const struct pl_swapchain_frame *frame)
 {
-    if (nk_convert(&ui->nk, &ui->cmds, &ui->verts, &ui->idx, &ui->convert_cfg) != NK_CONVERT_SUCCESS) {
-        return false;
-    }
+  if (nk_convert(&ui->nk, &ui->cmds, &ui->verts, &ui->idx, &ui->convert_cfg) != NK_CONVERT_SUCCESS) {
+      return false;
+  }
 
-    const struct nk_draw_command *cmd = NULL;
-    const uint8_t* vertices = reinterpret_cast<const uint8_t*>(nk_buffer_memory(&ui->verts));
-    const nk_draw_index* indices = reinterpret_cast<const nk_draw_index*>(nk_buffer_memory(&ui->idx));
-    nk_draw_foreach(cmd, &ui->nk, &ui->cmds) {
-        if (!cmd->elem_count)
-            continue;
+  const struct nk_draw_command *cmd = NULL;
+  const uint8_t* vertices = reinterpret_cast<const uint8_t*>(nk_buffer_memory(&ui->verts));
+  const nk_draw_index* indices = reinterpret_cast<const nk_draw_index*>(nk_buffer_memory(&ui->idx));
+  nk_draw_foreach(cmd, &ui->nk, &ui->cmds) {
+      if (!cmd->elem_count)
+          continue;
 
-        pl_shader sh = pl_dispatch_begin(ui->dp);
-        struct pl_shader_desc shader_desc = {
-            .desc = {
-                .name = "ui_tex",
-                .type = PL_DESC_SAMPLED_TEX,
-            },
-            .binding = {
-                .object = cmd->texture.ptr,
-                .sample_mode = PL_TEX_SAMPLE_NEAREST,
-            },
-        };
-        struct pl_custom_shader custom_shader = {
-            .description = "nuklear UI",
-            .body = "color = textureLod(ui_tex, coord, 0.0).r * vcolor;",
-            .output = PL_SHADER_SIG_COLOR,
-            .descriptors = &shader_desc,
-            .num_descriptors = 1,
-        };
-        pl_shader_custom(sh, &custom_shader);
+      pl_shader sh = pl_dispatch_begin(ui->dp);
+      struct pl_shader_desc shader_desc = {
+          .desc = {
+              .name = "ui_tex",
+              .type = PL_DESC_SAMPLED_TEX,
+          },
+          .binding = {
+              .object = cmd->texture.ptr,
+              .sample_mode = PL_TEX_SAMPLE_NEAREST,
+          },
+      };
+      struct pl_custom_shader custom_shader = {
+          .description = "nuklear UI",
+          .body = "color = textureLod(ui_tex, coord, 0.0).r * vcolor;",
+          .output = PL_SHADER_SIG_COLOR,
+          .descriptors = &shader_desc,
+          .num_descriptors = 1,
+      };
+      pl_shader_custom(sh, &custom_shader);
 
 
-        struct pl_color_repr repr = frame->color_repr;
-        struct pl_color_map_args cmap_args = {
-            .src = pl_color_space_srgb,
-            .dst = frame->color_space
-        };
-        pl_shader_color_map_ex(sh, NULL, &cmap_args);
-        pl_shader_encode_color(sh, &repr);
+      struct pl_color_repr repr = frame->color_repr;
+      struct pl_color_map_args cmap_args = {
+          .src = pl_color_space_srgb,
+          .dst = frame->color_space
+      };
+      pl_shader_color_map_ex(sh, NULL, &cmap_args);
+      pl_shader_encode_color(sh, &repr);
 
-        struct pl_dispatch_vertex_params vertex_params = {
-            .shader = &sh,
-            .target = frame->fbo,
-            .scissors = {
-                .x0 = static_cast<int>(cmd->clip_rect.x),
-                .y0 = static_cast<int>(cmd->clip_rect.y),
-                .x1 = static_cast<int>(cmd->clip_rect.x + cmd->clip_rect.w),
-                .y1 = static_cast<int>(cmd->clip_rect.y + cmd->clip_rect.h),
-            },
-            .blend_params = &pl_alpha_overlay,
-            .vertex_attribs = ui->attribs_pl,
-            .num_vertex_attribs = NUM_VERTEX_ATTRIBS,
-            .vertex_stride = sizeof(struct ui_vertex),
-            .vertex_position_idx = 0,
-            .vertex_coords = PL_COORDS_ABSOLUTE,
-            .vertex_flipped = frame->flipped,
-            .vertex_type = PL_PRIM_TRIANGLE_LIST,
-            .vertex_count = static_cast<int>(cmd->elem_count),
-            .vertex_data = vertices,
-            .index_data = indices,
-            .index_fmt = PL_INDEX_UINT32,
-        };
-        bool ok = pl_dispatch_vertex(ui->dp, &vertex_params);
-        if (!ok) {
-            return false;
-        }
+      struct pl_dispatch_vertex_params vertex_params = {
+          .shader = &sh,
+          .target = frame->fbo,
+          .scissors = {
+              .x0 = static_cast<int>(cmd->clip_rect.x),
+              .y0 = static_cast<int>(cmd->clip_rect.y),
+              .x1 = static_cast<int>(cmd->clip_rect.x + cmd->clip_rect.w),
+              .y1 = static_cast<int>(cmd->clip_rect.y + cmd->clip_rect.h),
+          },
+          .blend_params = &pl_alpha_overlay,
+          .vertex_attribs = ui->attribs_pl,
+          .num_vertex_attribs = NUM_VERTEX_ATTRIBS,
+          .vertex_stride = sizeof(struct ui_vertex),
+          .vertex_position_idx = 0,
+          .vertex_coords = PL_COORDS_ABSOLUTE,
+          .vertex_flipped = frame->flipped,
+          .vertex_type = PL_PRIM_TRIANGLE_LIST,
+          .vertex_count = static_cast<int>(cmd->elem_count),
+          .vertex_data = vertices,
+          .index_data = indices,
+          .index_fmt = PL_INDEX_UINT32,
+      };
+      bool ok = pl_dispatch_vertex(ui->dp, &vertex_params);
+      if (!ok) {
+          return false;
+      }
 
-        indices += cmd->elem_count;
-    }
+      indices += cmd->elem_count;
+  }
 
-    nk_clear(&ui->nk);
-    nk_buffer_clear(&ui->cmds);
-    nk_buffer_clear(&ui->verts);
-    nk_buffer_clear(&ui->idx);
-    return true;
+  nk_clear(&ui->nk);
+  nk_buffer_clear(&ui->cmds);
+  nk_buffer_clear(&ui->verts);
+  nk_buffer_clear(&ui->idx);
+  return true;
 }
 
 struct nk_context *ui_get_context(struct ui *ui)
 {
-    return &ui->nk;
+  return &ui->nk;
 }
 
 void render_ui(struct ui *ui) {
-    if (!ui)
-        return;
+  if (!ui)
+      return;
 
-    struct nk_context *ctx = ui_get_context(ui);
-    //const struct nk_rect bounds = nk_rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    const struct nk_rect bounds = nk_rect(0, 0, 1920, 1080); // ToDo get real window size
-    // hide background
-    nk_style_push_style_item(ctx, &ctx->style.window.fixed_background, nk_style_item_hide());
-    if (nk_begin(ctx, "FULLSCREEN", bounds, NK_WINDOW_NO_SCROLLBAR))
-    {
-       nk_layout_space_begin(ctx, NK_DYNAMIC, bounds.w, bounds.h );
-       nk_layout_space_push(ctx, nk_rect(100, 0, 100, 30));
-       // draw in screen coordinates
-       if (nk_button_label(ctx, "PS")) {
-           // event handling
-       }
+  struct nk_context *ctx = ui_get_context(ui);
+  //const struct nk_rect bounds = nk_rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  const struct nk_rect bounds = nk_rect(0, 0, 1920, 1080); // ToDo get real window size
+  // hide background
+  nk_style_push_style_item(ctx, &ctx->style.window.fixed_background, nk_style_item_hide());
+  if (nk_begin(ctx, "FULLSCREEN", bounds, NK_WINDOW_NO_SCROLLBAR))
+  {
+     nk_layout_space_begin(ctx, NK_STATIC, bounds.w, bounds.h );
 
-       nk_layout_space_end(ctx);
-    }
-    nk_end(ctx);
-    // restore background
-    nk_style_pop_style_item(ctx);
+     for (const auto& buttonType : buttonTypes) {
+         auto it = imageViewMap.find(buttonType);
+         if (it != imageViewMap.end()) {
+             VkImageView imageView = it->second;
+             nk_image btnImage = nk_image_ptr(imageView)
+
+             nk_layout_space_push(ctx, nk_rect(100, 600, 64, 64));
+             nk_button_image(ctx, btnImage);
+             nk_layout_space_end(ctx);
+         } else {
+              nk_layout_space_push(ctx, nk_rect(100, 600, 64, 64));
+              // draw in screen coordinates
+              if (nk_button_label(ctx, "PS")) {
+                  // event handling
+              }
+
+              nk_layout_space_end(ctx);
+         }
+     }
+
+     nk_layout_space_push(ctx, nk_rect(100, 0, 100, 30));
+     // draw in screen coordinates
+     if (nk_button_label(ctx, "Test render button")) {
+         // event handling
+     }
+
+     nk_layout_space_end(ctx);
+  }
+  nk_end(ctx);
+  // restore background
+  nk_style_pop_style_item(ctx);
 }
 
 extern "C"
@@ -800,7 +831,7 @@ JNIEXPORT jlong JNICALL Java_com_grill_placebo_PlaceboManager_nkCreateUI
 extern "C"
 JNIEXPORT void JNICALL Java_com_grill_placebo_PlaceboManager_nkStoreImageView
   (JNIEnv *env, jobject obj, jlong imageView, jint btnType, jlong placebo_vulkan) {
-  VkImageView vkImageView = reinterpret_cast<VkImageView>(static_cast<uint64_t>(imageView));
+  VkImageView* pVkImageView = reinterpret_cast<VkImageView*>(static_cast<uint64_t>(imageViewPtr));
   ButtonType buttonType = static_cast<ButtonType>(btnType);
   pl_vulkan vulkan = reinterpret_cast<pl_vulkan>(placebo_vulkan);
 
@@ -809,7 +840,7 @@ JNIEXPORT void JNICALL Java_com_grill_placebo_PlaceboManager_nkStoreImageView
       // If an entry exists, destroy the old VkImageView
       vkDestroyImageView(vulkan->device, it->second, nullptr);
   }
-  imageViewMap[buttonType] = vkImageView;
+  imageViewMap[buttonType] = *pVkImageView;
 }
 
 extern "C"
